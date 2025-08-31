dbdadd7fef4dc752ff6e332617d2065e1b26a68d
TharinduDesh <tharindukokiller@gmail.com>
2025-08-31 08:38:46 +0530

    fixing errors

--- chat-backend/routes/webauthnRoutes.js
+++ chat-backend/routes/webauthnRoutes.js
@@ -216,7 +216,7 @@ router.post("/verify-authentication", async (req, res) => {
       credentialPublicKey: credentialPublicKey,
       counter: parseInt(authenticator.counter) || 0,
       // Add transports as an empty array if needed
-      transports: [],
+      transports: authenticator.transports,
     };
 
     console.log("Prepared for verification:", {
